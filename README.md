4fddd0dfe7f8fddbb928077710c856bdab974e6c
nishantbasu <nishantbasu100@gmail.com>
2024-11-17 15:37:50 -0600

    All Changes

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# generator-lit-sass-rollup-openwc-starter
-A generator for setting up a starter frontend app using Lit Element, SASS and Rollup. Uses Open WC for testing.
+# Lit Elemnt SASS Rollup Open WC Starter Template
+
+Deployed App : https://hive-sks-c9da748e2491.herokuapp.com
+
+Code Climate Report : https://codeclimate.com/github/nishant-basu-tamu3/student_knowledge_system
+
+## Instructions
+* Install Yeoman globally: npm install -g yo
+* Then install this generator globally: npm install -g generator-lit-sass-rollup-openwc-starter
+* Navigate to a directory you want to generate the files in, then run yo lit-sass-rollup-openwc-starter
+* Follow the instructions
---
+++ generators/app/index.js
@@ -0,0 +1,120 @@
+import Generator from 'yeoman-generator';
+import { execSync } from 'child_process';
+
+export default class extends Generator {
+    constructor(args, opts) {
+        super(args, opts);
+        this.options['skipInstall'] = true;
+    }
+
+    initializing() {
+        this.log('Welcome to the Lit Element Sass Rollup Generator!!');
+    }
+
+    async prompting() {
+        this.answers = await this.prompt([
+            {
+                type: 'input',
+                name: 'install',
+                message: 'Would you like to install dependencies? (y/n)',
+                default: 'y',
+                validate: input => /^(y|yes|n|no)$/i.test(input) || 'Please enter y or n',
+            },
+        ]);
+
+        if (this._isAffirmative(this.answers.install)) {
+            Object.assign(this.answers, await this.prompt([
+                {
+                    type: 'input',
+                    name: 'build',
+                    message: 'Would you like to build the project? (y/n)',
+                    default: 'y',
+                    validate: input => /^(y|yes|n|no)$/i.test(input) || 'Please enter y or n',
+                },
+            ]));
+            if (this._isAffirmative(this.answers.build)) {
+                Object.assign(this.answers, await this.prompt([
+                    {
+                        type: 'input',
+                        name: 'serve',
+                        message: 'Would you like to start the app? (y/n)',
+                        default: 'y',
+                        validate: input => /^(y|yes|n|no)$/i.test(input) || 'Please enter y or n',
+                    },
+                ]));
+            }
+        }
+    }
+
+    writing() {
+        this.log('Copying template files...');
+        const filesToCopy = [
+            'tsconfig.json',
+            'web-test-runner.config.mjs',
+            'web-dev-server.config.mjs',
+            'rollup.config.mjs',
+            'README.template.md',
+            'README.md',
+            'postcss.config.mjs',
+            'package.json',
+            'package-lock.json',
+            'eslint.config.mjs',
+            '.prettierrc',
+            '.gitignore',
+            'web-dev-server',
+            'demo',
+            'src',
+        ];
+
+        filesToCopy.forEach(file => {
+            this.fs.copy(this.templatePath(file), this.destinationPath(file));
+        });
+        this.log('Files copied successfully.');
+    }
+
+    install() {
+        this._installDependencies();
+    }
+
+    _installDependencies() {
+        if (this._isAffirmative(this.answers.install)) {
+            this.log('Installing dependencies...');
+            try {
+                execSync('npm i', { stdio: 'inherit' });
+                this.log('Dependencies installed successfully.');
+
+                if (this._isAffirmative(this.answers.build)) {
+                    this.log('Building the project...');
+                    try {
+                        execSync('npm run build', { stdio: 'inherit' });
+                        this.log('Project built successfully.');
+
+                        if (this._isAffirmative(this.answers.serve)) {
+                            this.log('Starting the app...');
+                            try {
+                                execSync('npm run serve', { stdio: 'inherit' });
+                                this.log('App started successfully.');
+                            } catch (error) {
+                                this.log(`Error starting the app: ${error.message}`);
+                            }
+                        }
+                    } catch (error) {
+                        this.log(`Error building the project: ${error.message}`);
+                    }
+                }
+            } catch (error) {
+                this.log(`Error installing dependencies: ${error.message}`);
+            }
+        } else {
+            this.log('Skipping dependency installation.');
+        }
+    }
+
+    _isAffirmative(response) {
+        return /^(y|yes)$/i.test(response);
+    }
+
+    end() {
+        this.log('\nFinished generating!\n');
+    }
+}
